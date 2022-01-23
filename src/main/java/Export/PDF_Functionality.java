09b8d3be8e4925abc7d670b34169f4291ccdeebd
SCP079BEEP <can.dechert@gmx.de>
2022-01-23 19:02:08 +0100

    Export Sachen

--- src/main/java/Export/PDF_Functionality.java
+++ src/main/java/Export/PDF_Functionality.java
@@ -1,4 +1,33 @@
 package Export;
 
-public class PDF_Functionality {
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class PDF_Functionality
+{
+    public static void CreatePDF(String _text)
+    {
+        Document document = new Document();
+        try
+        {
+            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("C:\\HealthInformation.pdf")); // PFAD ANPASSEN!
+            document.open();
+            document.add(new Paragraph(_text));
+            document.close();
+            writer.close();
+        }
+        catch(DocumentException e)
+        {
+            e.printStackTrace();
+        }
+        catch(FileNotFoundException f)
+        {
+            f.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/Export/TXT_Functionality.java
@@ -0,0 +1,20 @@
+package Export;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class TXT_Functionality
+{
+    public static void CreateTXT(String _text) throws IOException
+    {
+        File file = new File("C:\\HealthInformation.txt"); // PFAD ANPASSEN!
+        FileWriter Fwriter = new FileWriter(file, true); // Boolean = true -> Es wird nix überschrieben, false -> Datei wird mit jedem Aufruf gewiped und nur der neue Text reingemacht
+        PrintWriter Pwriter = new PrintWriter(Fwriter);
+
+        Pwriter.println(_text);
+
+        Pwriter.close();
+    }
+}
