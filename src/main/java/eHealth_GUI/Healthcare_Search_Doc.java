61d4e77d0a9ab62c6d9b0d898d116c18cd29cfdf
sidra <abbasisidra5@gmail.com>
2022-01-29 18:08:28 +0100

    Dropdown menus for diseases

--- src/main/java/eHealth_GUI/Healthcare_Search_Doc.java
+++ src/main/java/eHealth_GUI/Healthcare_Search_Doc.java
@@ -44,7 +44,11 @@ public class Healthcare_Search_Doc extends JFrame {
 
 	/*For the reminder*/
 	int dropDownIndex;
-	JComboBox dropDown;
+	JComboBox generalIssuesdropDown;
+	JComboBox skinIssuesdropDown;
+	JComboBox jointIssuesdropDown;
+	JComboBox chronicIssuesdropDown;
+	JComboBox reminderdropDown;
 	int distance;
 	JDateChooser dateChooser;
 	JSpinField hour;
@@ -94,100 +98,107 @@ public class Healthcare_Search_Doc extends JFrame {
 		lblNewJgoodiesLabel.setBounds(236, 61, 216, 39);
 		contentPane.add(lblNewJgoodiesLabel);
 
-		/*Write Health Issues into Text field*/
+		/*Choose Health Issues from dropdown menus*/
 
 		JLabel lblYourHealthProblems = new JLabel("You need consultations for:");
 		lblYourHealthProblems.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblYourHealthProblems.setBounds(26, 129, 179, 17);
 		contentPane.add(lblYourHealthProblems);
 
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(26, 157, 228, 105);
-		contentPane.add(textArea);
+		/*General Issues*/
 
-		/*--------------------------------Appointment Panel----------------------------------*/
+		JLabel lblGeneralIssues = new JLabel("General Issues:");
+		lblGeneralIssues.setBounds(26, 157, 179, 17);
+		contentPane.add(lblGeneralIssues);
 
+		String[] generalIssues =
+				{"Cold", "Sickness", "Flu", "Migraines", "Head Ache", "Fever",
+						"Stomach Pain", "Covid", "Allergies", "Obesity"
 
-			/*Date picker*/
-		Date heute = new Date();
-		JLabel lbChooseDate = new JLabel("Date:");
-		lbChooseDate.setBounds(420, 129, 179, 17);
-		getContentPane().add(lbChooseDate);
+		};
 
-		dateChooser = new JDateChooser(heute);
-		dateChooser.getCalendarButton().addActionListener(new ActionListener() {
+		generalIssuesdropDown = new JComboBox(generalIssues);
+		generalIssuesdropDown.setBounds(26, 178, 113, 25);
+		getContentPane().add(generalIssuesdropDown);
+
+		generalIssuesdropDown.addActionListener(new ActionListener() {
+
 			public void actionPerformed(ActionEvent e) {
-
-
+				dropDownIndex =	generalIssuesdropDown.getSelectedIndex();
+				System.out.println(dropDownIndex);
 			}
-
 		});
-		dateChooser.setBounds(420, 150, 129, 25);
-		getContentPane().add(dateChooser);
+		/*Skin Issues*/
 
-		/*Time pickers*/
+		JLabel lblSkinIssues = new JLabel("Skin Issues:");
+		lblSkinIssues.setBounds(150, 157, 179, 17);
+		contentPane.add(lblSkinIssues);
 
-		lbTime = new JLabel("Time:");
-		lbTime.setBounds(420, 180, 50, 19);
-		getContentPane().add(lbTime);
+		String[] skinIssues =
+				{"Akne", "Rashes", "Itches", "Skin Disease", "Moles"
+				};
 
-		hour = new JSpinField();
-		hour.setBounds(420, 200, 50, 22);
-		getContentPane().add(hour);
+		skinIssuesdropDown = new JComboBox(skinIssues);
+		skinIssuesdropDown.setBounds(150, 178, 113, 25);
+		getContentPane().add(skinIssuesdropDown);
 
-		lbHour = new JLabel("Hours");
-		lbHour.setFont(new Font("Tahoma", Font.ITALIC, 8));
-		lbHour.setBounds(420, 218, 129, 19);
-		getContentPane().add(lbHour);
-
-		minute = new JSpinField();
-		minute.setBounds(470, 200, 50, 22);
-		getContentPane().add(minute);
-
-		lblNewLabel = new JLabel("Minutes");
-		lblNewLabel.setFont(new Font("Tahoma", Font.ITALIC, 8));
-		lblNewLabel.setBounds(470, 218, 50, 19);
-		getContentPane().add(lblNewLabel);
-
-		/*Reminder Dropdown*/
-
-		JLabel lbReminder = new JLabel("Reminder:");
-		lbReminder.setBounds(420, 240, 70, 19);
-		getContentPane().add(lbReminder);
-
-		String[] reminderList = {"1 week", "3 days", "1 hour", "10 minutes"};
-
-		dropDown = new JComboBox(reminderList);
-		dropDown.setBounds(420, 260, 100, 25);
-		getContentPane().add(dropDown);
-
-		dropDown.addActionListener(new ActionListener() {
+		skinIssuesdropDown.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
-				dropDownIndex =	dropDown.getSelectedIndex();
+				dropDownIndex =	skinIssuesdropDown.getSelectedIndex();
+				System.out.println(dropDownIndex);
+			}
+		});
+		/*Joint Issues*/
+
+		JLabel lblJointIssues = new JLabel("Joint Issues:");
+		lblJointIssues.setBounds(26, 210, 179, 17);
+		contentPane.add(lblJointIssues);
+
+		String[] jointIssues =
+				{"Back Pain", "Knee Pain", "Neck Pain", "Joint Pain"
+				};
+
+		jointIssuesdropDown = new JComboBox(jointIssues);
+		jointIssuesdropDown.setBounds(26, 231, 113, 25);
+		getContentPane().add(jointIssuesdropDown);
+
+		jointIssuesdropDown.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				dropDownIndex =	jointIssuesdropDown.getSelectedIndex();
 				System.out.println(dropDownIndex);
 			}
 		});
 
-		setHourMaximum(20);
-		setHourMinimum(10);
+		/*Chronic Issues*/
 
-		setMinuteMaximum(59);
-		setMinuteMinimum(00);
+		JLabel lblchronicIssues = new JLabel("Chronic Issues:");
+		lblchronicIssues.setBounds(150, 210, 113, 25);
+		contentPane.add(lblchronicIssues);
 
-		setDropDownMaximumRowCount(3);
+		String[] chronicIssues =
+				{"Asthma", "Blood Pressure", "Hay Fever", "Diabetes"
+				};
 
+		chronicIssuesdropDown = new JComboBox(chronicIssues);
+		chronicIssuesdropDown.setBounds(150, 231, 113, 25);
+		getContentPane().add(chronicIssuesdropDown);
 
+		chronicIssuesdropDown.addActionListener(new ActionListener() {
 
-
-		/*--------------------------------Appointment Panel End----------------------------------*/
+			public void actionPerformed(ActionEvent e) {
+				dropDownIndex =	chronicIssuesdropDown.getSelectedIndex();
+				System.out.println(dropDownIndex);
+			}
+		});
 
 
 		/*Select Distance of Search*/
 
 		JLabel lblDistanceOfSearch = new JLabel("Distance of search:");
 		lblDistanceOfSearch.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblDistanceOfSearch.setBounds(26, 294, 140, 17);
+		lblDistanceOfSearch.setBounds(26, 294, 140, 25);
 		contentPane.add(lblDistanceOfSearch);
 
 		String ChooseDistance[] = {"1km", "2km", "5km", "10km", "15km", "20km"};
@@ -233,6 +244,85 @@ public class Healthcare_Search_Doc extends JFrame {
 				}
 			}
 		});
+
+		/*--------------------------------Appointment Panel----------------------------------*/
+
+
+			/*Date picker*/
+		Date heute = new Date();
+		JLabel lbChooseDate = new JLabel("Date:");
+		lbChooseDate.setBounds(420, 129, 179, 25);
+		getContentPane().add(lbChooseDate);
+
+		dateChooser = new JDateChooser(heute);
+		dateChooser.getCalendarButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+
+			}
+
+		});
+		dateChooser.setBounds(420, 150, 129, 25);
+		getContentPane().add(dateChooser);
+
+		/*Time pickers*/
+
+		lbTime = new JLabel("Time:");
+		lbTime.setBounds(420, 180, 50, 19);
+		getContentPane().add(lbTime);
+
+		hour = new JSpinField();
+		hour.setBounds(420, 200, 50, 22);
+		getContentPane().add(hour);
+
+		lbHour = new JLabel("Hours");
+		lbHour.setFont(new Font("Tahoma", Font.ITALIC, 8));
+		lbHour.setBounds(420, 218, 129, 19);
+		getContentPane().add(lbHour);
+
+		minute = new JSpinField();
+		minute.setBounds(470, 200, 50, 22);
+		getContentPane().add(minute);
+
+		lblNewLabel = new JLabel("Minutes");
+		lblNewLabel.setFont(new Font("Tahoma", Font.ITALIC, 8));
+		lblNewLabel.setBounds(470, 218, 50, 19);
+		getContentPane().add(lblNewLabel);
+
+		/*Reminder Dropdown*/
+
+		JLabel lbReminder = new JLabel("Reminder:");
+		lbReminder.setBounds(420, 240, 70, 19);
+		getContentPane().add(lbReminder);
+
+		String[] reminderList = {"1 week", "3 days", "1 hour", "10 minutes"};
+
+		reminderdropDown = new JComboBox(reminderList);
+		reminderdropDown.setBounds(420, 260, 100, 25);
+		getContentPane().add(reminderdropDown);
+
+		reminderdropDown.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				dropDownIndex =	reminderdropDown.getSelectedIndex();
+				System.out.println(dropDownIndex);
+			}
+		});
+
+		setHourMaximum(20);
+		setHourMinimum(10);
+
+		setMinuteMaximum(59);
+		setMinuteMinimum(00);
+
+		setDropDownMaximumRowCount(3);
+
+
+
+
+		/*--------------------------------Appointment Panel End----------------------------------*/
+
+
 	}
 
 	/*Get and set methods for the appointment function*/
@@ -322,10 +412,10 @@ public class Healthcare_Search_Doc extends JFrame {
 	 * @return Integer Index of chosen element.
 	 */
 	public int getDropDownMaximumRowCount() {
-		return dropDown.getMaximumRowCount();
+		return reminderdropDown.getMaximumRowCount();
 	}
 	public void setDropDownMaximumRowCount(int maximumRowCount) {
-		dropDown.setMaximumRowCount(maximumRowCount);
+		reminderdropDown.setMaximumRowCount(maximumRowCount);
 	}
 
 }
