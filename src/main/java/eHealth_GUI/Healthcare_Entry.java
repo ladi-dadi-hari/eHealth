5b13779b86514e52f8a61f3842cdb3ffc61a4038
SCP079BEEP <can.dechert@gmx.de>
2022-02-08 21:10:33 +0100

    Hide on close bei Healthcare Entry

--- src/main/java/eHealth_GUI/Healthcare_Entry.java
+++ src/main/java/eHealth_GUI/Healthcare_Entry.java
@@ -45,7 +45,7 @@ public class Healthcare_Entry extends JFrame {
 	 * @param patient takes the object Patient that is created at the time of logging in
 	 */
 	public Healthcare_Entry(Patient patient) {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setBounds(100, 100, 355, 461);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
